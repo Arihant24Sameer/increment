2e7bc5445777ce72946d0e2ec9a9afff2602ffa8
arihant12 <arihant@antiersolutions.com>
2023-08-22 18:54:51 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+// App.js
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./actions/actions";
 import "./App.scss";
 const App = () => {
-  const count = useSelector((state) => state);
+  const count = useSelector((state) => state.counter); // Assuming 'counter' is the name of your reducer slice
   const dispatch = useDispatch();
 
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <h4>using React and Redux</h4>
       <div className="quantity">
         <button onClick={() => dispatch(decrement())}>-</button>
-        <input name="quantity" type="text" value={count} />
+        <input name="quantity" type="text" value={count} readOnly />
         <button onClick={() => dispatch(increment())}>+</button>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
-
-import { createStore } from "redux";
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./reducers/reducers";
 
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
 
 export default store;
